8cf44a9b9f663f0de2a206b8026abfdb414c8ce0
Evgeniy Zotov <u1359412@server199.hosting.reg.ru>
2021-04-22 00:42:10 +0300

    mylayout

---
+++ js/disable_scroll.js
@@ -0,0 +1,14 @@
+function disableScroll() {
+		let pagePosition = window.scrollY;
+		document.body.classList.add('disable-scroll');
+		document.body.dataset.position = pagePosition;
+		document.body.style.top = -pagePosition + 'px';
+	}
+
+function enableScroll() {
+		let pagePosition = parseInt(document.body.dataset.position, 10);
+		document.body.style.top = 'auto';
+		document.body.classList.remove('disable-scroll');
+		window.scroll({top: pagePosition, left: 0});
+		document.body.removeAttribute('data-position');
+}
